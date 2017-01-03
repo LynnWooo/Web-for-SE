e12a7910b0c709e027bde6a79be3e656bdb0a6eb
LynnWooo <Zion@ZiondeMacBook-Air.local>
2017-01-03 20:52:10 +0800

    non-data

---
+++ login.js
@@ -0,0 +1,7 @@
+var user_id=0
+var user_pwd=0
+
+function js_login(){
+	user_id=$('#login_name').value;
+	user_pwd=$('#login_pwd').value;
+}
---
+++ sign_up.js
@@ -0,0 +1,7 @@
+function sendCode(){
+
+}
+
+function subForm(){
+	
+}
