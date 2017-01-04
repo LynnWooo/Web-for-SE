97725bc5d5f69ade173a860cf19d466dc5fabf32
LynnWooo <Zion@ZiondeMacBook-Air.local>
2017-01-04 23:17:04 +0800

    sign up_non decorate

--- login.js
+++ login.js
@@ -2,6 +2,18 @@ var user_id=0
 var user_pwd=0
 
 function js_login(){
-	user_id=$('#login_name').value;
-	user_pwd=$('#login_pwd').value;
+	user_id=$("input[name='login_name']").val();
+	user_pwd=$("input[name='login_pwd']").val();
+	$.ajax({
+	url:'http://101.200.61.252:8080/user/login',
+	dataType:'json',
+	cache:false,
+	data:'identify='+user_id+'&password='+user_pwd,
+	type:'POST',
+	success:function(data){
+		
+		data.identify;
+		alert('success!');
+	}
+	});
 }
--- sign_up.js
+++ sign_up.js
@@ -1,7 +1,63 @@
-function sendCode(){
 
+function sendCode(){
+	var phoneNum;
+	phoneNum=$("input[name='phone_number']").val();
+	$.ajax({
+	url:'http://101.200.61.252:8080/user/getvertifycode',
+	dataType:'json',
+	cache:false,
+	data:'phoneNum='+phoneNum,
+	type:'POST',
+	success:function(){
+
+	}
+});
 }
 
 function subForm(){
-	
+	var temp=checkCode();
+	if(!temp)
+		return;
+	var id=$("input[name='user_id']").val();
+	var pwd=$("input[name='password']").val();
+	var name=$("input[name='user_name']").val();
+	var phone=$("input[name='phone_number']").val();
+	var code=$("input[name='veri_code']").val();
+	var json='id='+id+'&password='+pwd+'&name='+name+'&phoneNum='+phone+'&vertifyCode='+code+'&role=teacher';
+	//alert(json);
+	$.ajax({
+		url:'http://101.200.61.252:8080/user/register',
+		dataType:'json',
+		cache:false,
+		data:json,
+		type:'POST',
+		success:function(status){
+			if(status==0){
+				alert('注册成功！');
+				setTimeout("window.location.assign('login.html')",3000);
+			}else if(status==1){
+				$("#error_box").html('请填写完整数据！');
+			}else if(status==2){
+				$("#error_box").html('手机号码已存在！');
+			}else if(status==3){
+				$("#error_box").html('验证码错误！');
+			}else{
+				$("#error_box").html('发生未知错误');
+			}
+		}
+	});
+}
+
+function checkCode(){
+	var pwd_1;
+	var pwd_2;
+	pwd_1=$("input[name='password']").val();
+	pwd_2=$("input[name='password_2']").val();
+	if(pwd_1==pwd_2){
+		return true;
+	}
+	else{
+		$("#error_box").html('请再次确认密码！');
+		return false;
+	}
 }
