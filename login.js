7bd8b6c62f5b268d1c7ccbd7f54100a681f43e41
LynnWooo <Zion@ZiondeMacBook-Air.local>
2017-01-05 03:12:22 +0800

    view_no api

--- login.js
+++ login.js
@@ -11,9 +11,14 @@ function js_login(){
 	data:'identify='+user_id+'&password='+user_pwd,
 	type:'POST',
 	success:function(data){
-		
-		data.identify;
-		alert('success!');
+		if(data.status!=null)
+			window.location.assign('view.html');
+		else if(data.status==1)
+			$("#error_box").html('用户不存在');
+		else if(data.status==2)
+			$("#error_box").html('密码错误');
+		else
+			$("#error_box").html('未知错误');
 	}
 	});
 }
